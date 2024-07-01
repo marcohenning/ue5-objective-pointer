ca0350e7183ec9b33ed169fd858b7741f9ee7621
Marco Henning <marcohnngpersonal@gmail.com>
2024-07-01 13:45:10 +0200

    Set up basic project

---
+++ Source/ObjectivePointer/Character/FirstPersonCharacter.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FirstPersonCharacter.generated.h"
+
+
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+
+/**
+* Custom character using the default Unreal Engine 4 mannequin.
+*/
+UCLASS()
+class OBJECTIVEPOINTER_API AFirstPersonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFirstPersonCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	/** First person camera component */
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* FirstPersonCamera;
+
+	/** Character mapping context */
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* MappingContextCharacter;
+
+	/** Input action for basic movement */
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Input action for looking around */
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Input action for jumping */
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Crosshair widget class added to the viewport in beginplay */
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> CrosshairWidgetClass;
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+};
